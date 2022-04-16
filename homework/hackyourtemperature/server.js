9c61cda0e41be2daa5039996be907798feaa14a4
Furkan Cabuk <furkancabuk.ce@gmail.com>
2022-04-16 14:28:42 +0200

    app.use, location updated

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -3,11 +3,12 @@ import express from "express"
 const app = express()
 const PORT = 3000
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('hello from backend to frontend!!!')
 })
 
-app.use(express.json())
 app.post('/weather', (req, res) => {
   const cityName = req.body.cityName
   if (!cityName) {
